92059c69942410528dac80ee01f0d82e26d3598b
Daniel <lee4649@purdue.edu>
2025-01-04 01:58:59 -0500

    Reformatted PitchLookupModel

--- src/prm_vision/pnp_solver/include/PitchLookupModel.hpp
+++ src/prm_vision/pnp_solver/include/PitchLookupModel.hpp
@@ -1,21 +1,28 @@
+#ifndef PITCH_LOOKUP_MODEL_HPP
+#define PITCH_LOOKUP_MODEL_HPP
+
 #include <climits>
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 
 class PitchLookupModel {
-    public:
-        PitchLookupModel();
-        PitchLookupModel(std::string filename);
-        
-        void load_file(std::string filename);
-        float get_pitch(int distance, int height);
-    private:
-        int lower_x = INT_MAX;
-        int lower_z = INT_MAX;
-        int upper_x = INT_MIN;
-        int upper_z = INT_MIN;
-        std::vector<std::vector<float>> pitch_lookup_table;
+public:
+    PitchLookupModel();
+    explicit PitchLookupModel(std::string filename);
 
-        float map(float value, float old_min, float old_max, float new_min, float new_max);
+    void load_file(std::string filename);
+    float get_pitch(int distance, int height);
+
+private:
+    int lower_x = INT_MAX;
+    int lower_z = INT_MAX;
+    int upper_x = INT_MIN;
+    int upper_z = INT_MIN;
+    std::vector<std::vector<float>> pitch_lookup_table;
+
+    float map(float value, float old_min, float old_max, float new_min, float new_max);
 };
+
+#endif // PITCH_LOOKUP_MODEL_HPP
